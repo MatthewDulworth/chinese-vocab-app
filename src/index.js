f0dd1e74691e0c4f4c2e4c58d659b6733fb74e57
Matthew Dulworth <mhdulworth@gmail.com>
2021-06-08 09:19:50 -0500

    removed easybase package, config, wrapper in index.js

--- src/index.js
+++ src/index.js
@@ -2,14 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import {EasybaseProvider} from 'easybase-react';
-import ebconfig from './ebconfig';
 
 ReactDOM.render(
   <React.StrictMode>
-    <EasybaseProvider ebconfig={ebconfig}>
-      <App />
-    </EasybaseProvider>
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
